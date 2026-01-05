53d3c3b7e287f6043ea1b091ffc0e5405a5e1eb0
lee-eojin <lejin0906@snu.ac.kr>
2026-01-05 21:06:41 +0900

    refactor: correct menu recommendation logic

--- src/InputView.js
+++ src/InputView.js
@@ -1,16 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGE } from './constants.js';
 
 class InputView {
     async readUserNames() {
         const input = await Console.readLineAsync(
-            "코치의 이름을 입력해 주세요. (, 로 구분)\n"
+            INPUT_MESSAGE.COACH_NAME
         );
         return input.split(",").map(name => name.trim());
     }
 
     async readBlackListMenu(coachName) {
         const input = await Console.readLineAsync(
-            `${coachName}(이)가 못 먹는 메뉴를 입력해 주세요.\n`
+            `${coachName}${INPUT_MESSAGE.BLACKLIST_TEMPLATE}`
         );
         if (!input) return [];
         return input.split(",").map(menu => menu.trim());
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,22 +1,24 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console } from "@woowacourse/mission-utils";
+import { OUTPUT_MESSAGE } from "./constants.js";
 
 class OutputView {
-    printStart() {
-        Console.print("점심 메뉴 추천을 시작합니다.\n");
-    }
-    printResult(coaches, categories, menus) {
-        Console.print("메뉴 추천 결과입니다.");
-        Console.print("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
-        Console.print(`[ 카테고리 | ${categories.join(" | ")} ]`);
+  printStart() {
+    Console.print(OUTPUT_MESSAGE.START);
+  }
+  printResult(coaches, categories, menus) {
+    Console.print(OUTPUT_MESSAGE.RECOMMEND_RESULT);
+    Console.print(OUTPUT_MESSAGE.DAY_INDEX);
+    Console.print(
+      `${OUTPUT_MESSAGE.CATEGORIES_TEMPLATE}${categories.join(" | ")} ]`
+    );
 
-        coaches.forEach(coach => {
-            Console.print(`[ ${coach} | ${menus[coach].join(" | ")} ]`);
-        });
-    }
-    printComplete() {
-        Console.print("\n추천을 완료했습니다.");
-    }
+    coaches.forEach((coach) => {
+      Console.print(`[ ${coach} | ${menus[coach].join(" | ")} ]`);
+    });
+  }
+  printComplete() {
+    Console.print(OUTPUT_MESSAGE.COMPLETE);
+  }
 }
-
 
 export default OutputView;
